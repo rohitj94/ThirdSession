527e92e560836b800efaa0ad8715416d31231a3d
rohitj94 <rohitj94@gmail.com>
2016-08-29 01:36:33 +0530

    Assignment1 of OOPS-1

---
+++ CubeRootProgram.java
@@ -0,0 +1,13 @@
+import java.lang.Math.*;
+
+class CubeRootProgram
+ 
+{
+   public static void main(String agrs[])
+   {
+     int a = 26;
+
+     System.out.println("Cube root of Number : " + Math.cbrt(a));
+     System.out.println("Cube root of Number : " + Math.sqrt(a));
+   }
+}
